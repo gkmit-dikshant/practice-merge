f4de07af095ce439bdfcfd2705ffc6b52585dcff
gkmit-dikshant <dikshant@gkmit.co>
2025-10-16 10:33:04 +0530

    feat: added stdc++ file

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+#include "stdc++.h"
+// #include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    vector<int>res;
+    priority_queue<int> pq;
+    unordered_set<int>st;
+cout << "Hello world" << endl;
+return 0;
+}
